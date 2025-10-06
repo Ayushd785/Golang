13871a1f05af917eb23311d9b8494ffd9a4c8b00
Ayushd785 <ayushd785@gmail.com>
2025-10-06 11:23:15 +0530

    type conversion variables

---
+++ TourOfGo/Basics/Variables/var.go
@@ -0,0 +1,23 @@
+package main
+import (
+	"fmt"
+)
+var python , c, java bool
+//Declare var       // Declare the data type
+
+
+var t, j int = 1,2
+func main(){
+	var i int =4
+	var hello,bye,welcome = true, false, true
+	fmt.Println(i,java,python,c)
+	fmt.Println(hello,bye,welcome,t,j)
+
+
+
+	//Inside a function, the := short assignment statement can be used in place of a var declaration with implicit type.
+
+	K := 5
+	fmt.Println(K)
+
+}
